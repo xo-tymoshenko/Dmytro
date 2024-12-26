16a31f9590a2d69645e8c9fd23dd00e64065852b
Serhii Tymoshenko <xo.tymoshenko@gmail.com>
2024-12-26 11:06:25 +0100

    Removed extra code

--- app/src/main/java/dev/tymoshenko/dmytro/ui/components/ScrollingScreen.kt
+++ app/src/main/java/dev/tymoshenko/dmytro/ui/components/ScrollingScreen.kt
@@ -1,31 +1,16 @@
 package dev.tymoshenko.dmytro.ui.components
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.expandVertically
-import androidx.compose.animation.fadeIn
 import androidx.compose.animation.shrinkVertically
-import androidx.compose.animation.slideIn
-import androidx.compose.animation.slideInVertically
-import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clipToBounds
-import androidx.compose.ui.layout.onGloballyPositioned
-import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
 import dev.tymoshenko.dmytro.data.models.NavStatus
 
 @Composable
@@ -35,27 +20,6 @@ fun ScrollingScreen(
     bottomBar: @Composable (() -> Unit)? = null,
     content: @Composable () -> Unit
 ) {
-    val topBarHeightState = remember { mutableStateOf(0.dp) }
-    val bottomBarHeightState = remember { mutableStateOf(0.dp) }
-
-    val topBarOffset = animateDpAsState(
-        targetValue = when (navStatus) {
-            NavStatus.SHOWN -> 0.dp
-            NavStatus.HIDDEN -> topBarHeightState.value
-        },
-        label = "null",
-        animationSpec = tween(500)
-    )
-
-    val bottomBarOffset = animateDpAsState(
-        targetValue = when (navStatus) {
-            NavStatus.SHOWN -> 0.dp
-            NavStatus.HIDDEN -> bottomBarHeightState.value
-        },
-        label = "null",
-        animationSpec = tween(500)
-    )
-
     ScrollingScreenContent(
         topBar = topBar,
         navStatus = navStatus,
@@ -72,7 +36,6 @@ private fun ScrollingScreenContent(
     bottomBar: @Composable (() -> Unit)? = null,
     content: @Composable () -> Unit
 ) {
-    val density = LocalDensity.current
 
     Column(
         modifier = Modifier
@@ -81,14 +44,14 @@ private fun ScrollingScreenContent(
             .clipToBounds()
     ) {
         androidx.compose.animation.AnimatedVisibility(
-            visible = if (navStatus == NavStatus.SHOWN) true else false,
+            visible = navStatus == NavStatus.SHOWN,
             enter = expandVertically(
                 // Expand from the top.
                 expandFrom = Alignment.Top,
                 animationSpec = tween(500)
             ),
             exit = shrinkVertically(animationSpec = tween(500)),
-        ){
+        ) {
             topBar?.invoke()
         }
 
@@ -102,13 +65,13 @@ private fun ScrollingScreenContent(
         }
 
         androidx.compose.animation.AnimatedVisibility(
-            visible = if (navStatus == NavStatus.SHOWN) true else false,
-            enter = slideInVertically() + expandVertically(
+            visible = navStatus == NavStatus.SHOWN,
+            enter = expandVertically(
                 expandFrom = Alignment.Bottom,
                 animationSpec = tween(500)
             ),
             exit = shrinkVertically(animationSpec = tween(500)),
-        ){
+        ) {
             bottomBar?.invoke()
         }
     }
